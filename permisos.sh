2be8024098385eb01c7c9a40ecc770a0481b95dd
gabriel <levitarmouse@gmail.com>
2017-04-23 20:40:29 -0300

    Pruebas integración !preAlpha

--- permisos.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-/usr/bin/find . -name tmp -print0 | xargs -0 chmod -R 777;
-/usr/bin/find . -name cache -print0 | xargs -0 chmod -R 777;
-/usr/bin/find . -name logs -print0 | xargs -0 chmod -R 777;
